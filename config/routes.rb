59f33d85438ebae2e668737842fcb9e3fa60208f
EgimenoS <ergisan@gmail.com>
2020-10-24 17:54:56 +0200

    override api_guard RegistrationControllerto allow saving the name of the user in registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  api_guard_routes for: 'users'
+  api_guard_routes for: 'users', controller: {
+    registration: 'users/registration',
+    authentication: 'users/authentication',
+    passwords: 'users/passwords',
+    tokens: 'users/tokens'
+  }
 end
