8dfb4f00a0ca909ccffcaea2e0c97b6f93bcd204
EgimenoS <ergisan@gmail.com>
2020-10-27 22:03:49 +0100

    added extra validations for User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@
 
 class User < ApplicationRecord
   has_secure_password
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :name, on: :create
 end
