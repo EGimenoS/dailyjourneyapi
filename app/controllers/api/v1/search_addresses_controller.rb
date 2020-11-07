62e955e9e9c8c583198b27066847bea2478298f0
EgimenoS <ergisan@gmail.com>
2020-11-08 00:28:16 +0100

    Refactor: Created Adapter module and moved HereMaps api calls to its adapter class

--- app/controllers/api/v1/search_addresses_controller.rb
+++ app/controllers/api/v1/search_addresses_controller.rb
@@ -2,11 +2,8 @@
 
 class Api::V1::SearchAddressesController < Api::V1::BaseController
   def index
-    apikey = ENV['here_maps_api_key']
-    limit = 5
-    config = { apikey: apikey, limit: limit }
-    req_params = { at: params['at'], q: params['q'] }
-    results = RestClient.get 'https://discover.search.hereapi.com/v1/discover', { params: config.merge(req_params) }
+    url = 'https://discover.search.hereapi.com/v1/discover'
+    results = Adapter::HereMaps.new(url, params['q'], params['at']).autosuggest_addresses
     render_object(results, :ok)
   end
 end
