74fa5155c1b58601581b72e287c678ef04ffd723
EgimenoS <ergisan@gmail.com>
2020-11-19 20:02:05 +0100

    aded name of the user to the jwt payload

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: true, presence: true
   validates_presence_of :name, on: :create
+
+  def jwt_token_payload
+    { name: name }
+  end
 end
