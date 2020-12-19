1d36b26329aab087e41e3a9556ab71111dd5c251
EgimenoS <ergisan@gmail.com>
2020-12-19 18:30:15 +0100

    fixed query in profile_travels#index

--- app/controllers/api/v1/profile_travels_controller.rb
+++ app/controllers/api/v1/profile_travels_controller.rb
@@ -3,7 +3,7 @@
 class Api::V1::ProfileTravelsController < Api::V1::BaseController
   before_action :authenticate_and_set_user
   def index
-    travels = Travel.joins(:participants).where('participants.user_id' => current_user.id).or(Travel.joins(:participants).where('travels.owner_id' => current_user.id))
+    travels = Travel.left_joins(:participants).where('participants.user_id' => current_user.id).or(Travel.left_joins(:participants).where('travels.owner_id' => current_user.id))
     render json: travels, status: :ok, each_serializer: TravelDetailSerializer
   end
 end
