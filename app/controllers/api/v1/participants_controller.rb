393b1d7da9716788bbdeef19a61da1aee7091a1a
EgimenoS <ergisan@gmail.com>
2020-12-26 13:57:38 +0100

    fixed typo

--- app/controllers/api/v1/participants_controller.rb
+++ app/controllers/api/v1/participants_controller.rb
@@ -3,7 +3,7 @@
 class Api::V1::ParticipantsController < Api::V1::BaseController
   before_action :authenticate_and_set_user
   before_action :set_participant, only: %i[destroy update]
-  before_action :require_authorized_for_current_participant, only: %i[create destroy update]
+  before_action :require_authorized_for_current_participant, only: %i[destroy update]
   def create
     participant = current_user.participants.build(participant_params)
     if participant.save
